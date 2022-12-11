11462c43ceab3831177d8c7f0cbbe827ebf15d92
Andrey <scootaloorainbow@yandex.ru>
2022-12-11 17:06:28 +0300

    fix: remove sidebar block title css

--- src/ui/SideBarBlockTitle/SideBarBlockTitle.tsx
+++ src/ui/SideBarBlockTitle/SideBarBlockTitle.tsx
@@ -1,7 +1,5 @@
 import type { SideBarBlockTitleProps } from "./SideBarBlockTitleProps.typedef";
 
-import "./SidebarBlockTitle.css";
-
 const SideBarBlockTitle = ({ text }: SideBarBlockTitleProps) => {
   return <h2 className="text-2xl w-[100%] text-white font-bold">{text}</h2>;
 };
