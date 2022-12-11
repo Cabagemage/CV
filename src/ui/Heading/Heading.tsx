23356d264fe54870a50e1e84c9a33fbf42afd8b2
Andrey Zaitsev <63799449+Cabagemage@users.noreply.github.com>
2022-12-11 14:50:58 +0300

    Fix 3 (#5)
    * fix: rm sidebar from import
    
    * feat: move heading to sidebar

--- src/ui/Heading/Heading.tsx
+++ src/ui/Heading/Heading.tsx
@@ -3,7 +3,7 @@ import "./Heading.css";
 import type { HeadingProps } from "./Heading.typedef";
 
 export const Heading: FC<HeadingProps> = ({ title, subtitle }) => (
-  <section className="heading">
+  <section className="heading text-white">
     <h1 className="heading__title">{title}</h1>
     <h2 className="heading__subtitle">{subtitle}</h2>
   </section>
