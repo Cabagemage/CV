f55634ada07f2593e58b804ac2544e52dcdda263
Andrey <scootaloorainbow@yandex.ru>
2022-12-11 14:54:21 +0300

    fix: tsconfig

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+export { Achievements } from "./Achievements";
 export { Contacts } from "./Contacts";
+export { SideBar } from "./SideBar";
 export { Tags } from "./Tags";
 export { WorkPlaces } from "./WorkPlaces";
